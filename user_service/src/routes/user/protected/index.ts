0adb014f3613cc00307567c4cde7aeb920ffff28
sudhay23 <sudhay2001@gmail.com>
2024-05-12 10:18:43 +0530

    Get user profile route

--- user_service/src/routes/user/protected/index.ts
+++ user_service/src/routes/user/protected/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import updateProfileController from "../../../controllers/user/protected/updateProfileController";
 import toggleIsInvestorController from "../../../controllers/user/protected/toggleIsInvestorController";
+import getProfileController from "../../../controllers/user/protected/getProfileController";
 
 const router = express.Router();
 
@@ -9,5 +10,8 @@ router.put("/updateprofile", updateProfileController);
 
 // PUT /toggleisinvestor
 router.put("/toggleisinvestor", toggleIsInvestorController);
+
+// GET /profile
+router.get("/profile", getProfileController);
 
 export default router;
